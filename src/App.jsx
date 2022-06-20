863284cf4e75d38fa825c9112dcac472e9c1ad98
“Mohammad <mohammadaliadnanrauf@gmail.com>
2022-06-20 16:36:05 +0500

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ff0e1642c3bd18b053cd952c6ad14cb5b76c32ef4dc1a6c34db8f7446e65cb2
-size 740
+oid sha256:9783faac13a9d251b1af10665f63bcdc8fb5c02081a6f09e3e136b5f6b137971
+size 751
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e71099732c324301f2ac106b8095c0683c31763ebae20575f1fc00d1721afabb
-size 924
+oid sha256:6c8d89c51bb4740a1c4d497770ad3d1afda4ab54b945cf9e0a2b4cae8b229e5f
+size 995

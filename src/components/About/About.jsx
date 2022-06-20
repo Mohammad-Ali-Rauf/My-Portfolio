05d64eb33cdd62158ad6fd40e21de8e7035f6b4c
“Mohammad <mohammadaliadnanrauf@gmail.com>
2022-06-20 09:58:10 +0500

    -

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7225c4890aac006a5fb975f6772f9e3dffd24c3daf15f84f11d97103584f4ac
-size 1693
+oid sha256:b4ccc45baac5d91e0c0d42f9759216b786e199dfb6e842e374f2dac9d7a999e1
+size 1605

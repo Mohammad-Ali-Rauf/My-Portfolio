8bac724e7351e2350d170c480ae961c65db0d1f3
“Mohammad <mohammadaliadnanrauf@gmail.com>
2022-06-20 10:10:09 +0500

    -

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a8508e9cacef9fc7b5df23441fd3c269fffbf0402ecc84e38c1981ea134d9d2
-size 949
+oid sha256:065969b384db94266a8d58e3d958280e20a64069495af8ec9aeccfc898a8daa1
+size 1073

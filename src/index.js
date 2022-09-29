c9c104c1a106bda31f31d8fb4316455adb742319
Mohammad Ali <m.aliadnanrauf@gmail.com>
2022-09-29 14:02:32 +0500

    -

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4c852ce9293d9191190eea2d8d312c6bdffede3ecec190c61231c30e068974a
+size 260

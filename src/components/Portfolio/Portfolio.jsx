3e427351699e6c84ba9bcffc8d44937e67cb93f2
Mohammad Ali <m.aliadnanrauf@gmail.com>
2023-04-16 09:53:49 +0500

    Portfolio Updated

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import GithubFinderImg from '../../assets/github-finder.png';
 import VideoChatImg from '../../assets/video-chat.png';
-import ContactManagementImg from '../../assets/contact-management.png';
-import BlogAppImg from '../../assets/blog-app.png'
+import SeatSelectorImg from '../../assets/seat-selector.png';
+import FormValidationImg from '../../assets/form-validation.png';
 
 // Import Styles
 import './Portfolio.css';
@@ -13,6 +13,8 @@ const data = [
     id: 1,
     image: GithubFinderImg,
     title: 'Github Users Finder',
+    completed: false,
+    skillsUsed: ['REACT JS', 'HTML5', 'CSS3'],
     github: 'https://github.com/Mohammad-Ali-Rauf/Github-Finder',
     live: 'https://github-finder-ali.netlify.app',
   },
@@ -20,23 +22,29 @@ const data = [
     id: 2,
     image: VideoChatImg,
     title: 'Video Chat',
+    completed: false,
+    skillsUsed: ['REACT JS ', 'HTML5', 'MATERIAL UI'],
     github: 'https://github.com/Mohammad-Ali-Rauf/Video-Chat',
     live: 'https://video-chat-app-ali.netlify.app',
   },
   {
     id: 3,
-    image: ContactManagementImg,
-    title: 'Contact Manager',
-    github: 'https://github.com/Mohammad-Ali-Rauf/contact-management-frontend',
-    live: 'https://contact-manager-ali.netlify.app',
+    image: SeatSelectorImg,
+    title: 'Movie Seat Selector',
+    completed: false,
+    skillsUsed: ['HTML5', 'CSS3', 'JAVASCRIPT'],
+    github: 'https://github.com/Mohammad-Ali-Rauf/Seat-Selector',
+    live: 'https://movie-seat-selector.netlify.app/',
   },
   {
     id: 4,
-    image: BlogAppImg,
-    title: 'Blog App',
-    github: 'https://github.com/Mohammad-Ali-Rauf/my-blog',
-    live: 'https://contact-manager-ali.netlify.app',
-  }
+    image: FormValidationImg,
+    title: 'HTML Form Validation',
+    completed: true,
+    skillsUsed: ['HTML5', 'CSS3', 'JAVASCRIPT'],
+    github: 'https://github.com/Mohammad-Ali-Rauf/html-form-validation',
+    live: 'https://html-form-validation-ali.netlify.app'
+  },
 ]
 
 const Portfolio = () => {
@@ -44,14 +52,15 @@ const Portfolio = () => {
     <section id='portfolio'>
       <h5>My Recent Projects</h5>
       <h2>Portfolio</h2>
-      
+
       <div className="container portfolio__container">
-        {data.map(({id, image, title, github, live}) => (
+        {data.map(({ id, image, title, completed, skillsUsed, github, live }) => (
           <article key={id} className='portfolio__item'>
             <div className="portfolio__item-image">
               <img src={image} alt={title} />
             </div>
             <h3>{title}</h3>
+            <h4 style={{ marginBottom: '25px', }}>Skills Used: <span style={{ color: 'lightgrey' }}>{String(skillsUsed.join(', '))}</span></h4>
             <div className="portfolio__item-cta">
               <a href={github} className='btn' target='_blank' rel='noopener noreferrer'>Github Repo</a>
               <a href={live} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>Live App</a>

dd8874adb474192963ca2e0c267955975e999ce9
Mohammad Ali <m.aliadnanrauf@gmail.com>
2023-05-30 00:59:28 +0500

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import { AiOutlineUser } from 'react-icons/ai';
 import { BiBook } from 'react-icons/bi';
 import { MdMiscellaneousServices } from 'react-icons/md';
 import { BiMessageSquareDetail } from 'react-icons/bi';
+import { GiGraveyard } from 'react-icons/gi';
+import { FaBriefcase } from 'react-icons/fa';
 
 // Import Styles
 import './Navbar.css';
@@ -18,6 +20,8 @@ const Navbar = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><BiBook /></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdMiscellaneousServices /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><FaBriefcase /></a>
+      <a href="#graveyard" onClick={() => setActiveNav('#graveyard')} className={activeNav === '#graveyard' ? 'active' : ''}><GiGraveyard /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
     </nav>
   )

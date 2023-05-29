0a4abe289a8ca5b913d27e5767d2edc488b3939f
Mohammad Ali <m.aliadnanrauf@gmail.com>
2023-05-29 20:14:26 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
-// import Testimonials from './components/Testimonials/Testimonials';
+import Testimonials from './components/Testimonials/Testimonials';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
@@ -18,7 +18,7 @@ const App = () => {
       <Skills />
       <Services />
       <Portfolio />
-      {/* <Testimonials /> */}
+      <Testimonials />
       <Contact />
       <Footer />
     </>

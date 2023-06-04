d13450c29b8a7b6734483f3c435a83e044cb3e90
Mohammad Ali <m.aliadnanrauf@gmail.com>
2023-06-04 21:12:42 +0500

    -

--- src/components/Graveyard/Graveyard.jsx
+++ src/components/Graveyard/Graveyard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e35b100383c4f786c6a804bfde7589e436eae89732b97feacbeca144bc8b2425
-size 2325
+oid sha256:479c2d60a0332ead151cb44162c44630eba4c3f3ce7350be6724f05cde3271de
+size 2307
